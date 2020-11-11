2d37f54216f82636ea7f73f3c6ed506ef2ae46b6
etw11241 <etw11241@aol.com>
2020-11-10 22:19:07 -0500

    Infinite Code
    Infinite Code

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/mainBool.js
@@ -0,0 +1,8 @@
+function myBoolFunction1() {
+    document.write(5 < 4);
+}
+
+
+function myBoolFunction2() {
+    document.write(100 > 2);
+}
---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/mainInfinite.js
@@ -0,0 +1,10 @@
+function myInfiniteFunction1() {
+    var x = 2E310;
+    document.getElementById("Infinite1").innerHTML = x;
+}
+
+
+function myInfiniteFunction2() {
+    var y = -3E310;
+    document.getElementById("Infinite2").innerHTML = y;
+}
