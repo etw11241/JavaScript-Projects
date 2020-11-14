488d42a5c57e946948529ab4dc3141cb09bb982e
etw11241 <etw11241@aol.com>
2020-11-13 23:24:42 -0500

    And Or operators
    And Or operators

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/mainAndOr.js
@@ -0,0 +1,18 @@
+function myAndOrFunction1() {
+    document.write(9 > 6 && 3 < 11);
+}
+
+
+function myAndOrFunction2() {
+    document.write(2 > 9 && 8 > 1);
+}
+
+
+function myAndOrFunction3() {
+    document.write(1 < 10 || 13 > 5);
+}
+
+
+function myAndOrFunction4() {
+    document.write(2 > 8 || 15 < 3);
+}
