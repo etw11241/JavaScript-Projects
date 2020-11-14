467e46168e0f22146f5d5bee52736893059ade4b
etw11241 <etw11241@aol.com>
2020-11-14 16:10:19 -0500

    JS Not function
    JS Not function

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/mainNot.js
@@ -0,0 +1,7 @@
+function myNotFunction1() {
+    document.getElementById("Not1").innerHTML = !(3 > 12);
+}
+
+function myNotFunction2() {
+    document.getElementById("Not2").innerHTML = !(23 < 2);
+}
